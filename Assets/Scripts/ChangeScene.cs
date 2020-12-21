d9a7365930cfd10bb9c170138e853000b446e10d
Carlos Rodas <crods57@gmail.com>
2020-12-21 11:27:06 -0500

    Menú Principal

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    int scene;
+
+    public void ChangeToSelectedScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
