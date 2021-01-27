a17b514f3c4c6f8c68436bce3bd3e166b1c46082
Carlos Rodas <crods57@gmail.com>
2021-01-27 13:17:36 -0500

    Nueva transición

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -8,7 +8,7 @@ public class ItemSlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log("On Drop");
+        //Debug.Log("On Drop");
         if(eventData.pointerDrag != null)
         {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
