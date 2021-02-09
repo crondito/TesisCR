9e0a53afc8ab0bac100fa1248f04edeb617fb56d
Carlos Rodas <crods57@gmail.com>
2021-02-09 13:54:50 -0500

    SB player and box

--- Assets/Scripts/Sokoban/SBBox.cs
+++ Assets/Scripts/Sokoban/SBBox.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SBBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool Move(Vector2 direction)
     {
-        
+        if (BoxBlocked(transform.position, direction))
+        {
+            return false;
+        } else
+        {
+            transform.Translate(direction);
+            // TestForOnCross();
+            return true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    bool BoxBlocked(Vector3 position, Vector2 direction)
     {
-        
+        Vector2 newPos = new Vector2(position.x, position.y) + direction;
+        GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall");
+        foreach (var wall in walls)
+        {
+            if (wall.transform.position.x == newPos.x && wall.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        foreach (var box in boxes)
+        {
+            if (box.transform.position.x == newPos.x && box.transform.position.y == newPos.y)
+            {
+                return true;
+            }
+        }
+        return false;
     }
+
 }
