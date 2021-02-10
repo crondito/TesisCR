9a2adcfea743abf76c5fccf4fca79f477b522ec0
Carlos Rodas <crods57@gmail.com>
2021-02-10 16:54:21 -0500

    Sokoban reset and next buttons

--- Assets/Scripts/Sokoban/SBGameManager.cs
+++ Assets/Scripts/Sokoban/SBGameManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class SBGameManager : MonoBehaviour
 {
-
+    public SBLevelBuilder m_LevelBuilder;
+    public GameObject m_NextButton;
     private bool m_ReadyForInput;
-    public SBPlayer m_Player;
+    private SBPlayer m_Player;
+
+    private void Start()
+    {
+        m_LevelBuilder.Build();
+        m_Player = FindObjectOfType<SBPlayer>();
+    }
 
     private void Update()
     {
@@ -18,11 +25,24 @@ public class SBGameManager : MonoBehaviour
             {
                 m_ReadyForInput = false;
                 m_Player.Move(moveInput);
-                // m_NextButton.SetActive(IsLevelComplete());
+                //m_NextButton.SetActive(IsLevelComplete());
             }
         } else
         {
             m_ReadyForInput = true;
         }
+    }
+
+    public void NextLevel()
+    {
+        // m_NextButton.SetActive(false);
+        m_LevelBuilder.NextLevel();
+        m_LevelBuilder.Build();
+        // StartCoroutine(ResetSceneAsync());
+    }
+
+    public void ResetScene()
+    {
+        // StartCoroutine(ResetSceneAsync());
     }
 }
