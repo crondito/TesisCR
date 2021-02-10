7f1d17516ae580dfeb1a55f146ae2a46a6fa36ca
Carlos Rodas <crods57@gmail.com>
2021-02-10 13:02:34 -0500

    Sokaban Load Levels

--- Assets/Scripts/Sokoban/SBLevelBuilder.cs
+++ Assets/Scripts/Sokoban/SBLevelBuilder.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SBLevelBuilder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
