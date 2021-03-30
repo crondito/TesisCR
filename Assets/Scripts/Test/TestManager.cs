8a913f96e38f93108b849b9049afb55bbe31f8fa
Carlos Rodas <crods57@gmail.com>
2021-03-30 11:29:30 -0500

    Scripts para las nuevas pruebas

---
+++ Assets/Scripts/Test/TestManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestManager : MonoBehaviour
+{
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public Text QuestionTxt;
+
+    void Start()
+    {
+        GenerateQuestion();
+    }
+
+    public void Correct()
+    {
+        QnA.RemoveAt(currentQuestion);
+        GenerateQuestion();
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer == i + 1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+    }
+
+    void GenerateQuestion()
+    {
+        currentQuestion = Random.Range(0, QnA.Count);
+        QuestionTxt.text = QnA[currentQuestion].Question;
+        SetAnswers();
+
+    }
+}
