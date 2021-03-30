aa70aebdfb60886b9ef66ce09f0e7e3973194182
Carlos Rodas <crods57@gmail.com>
2021-03-30 12:59:45 -0500

    Preguntas en prueba funciona
    Falta que reinicie al terminar

--- Assets/Scripts/Test/TestManager.cs
+++ Assets/Scripts/Test/TestManager.cs
@@ -29,7 +29,7 @@ public class TestManager : MonoBehaviour
             options[i].GetComponent<AnswerScript>().isCorrect = false;
             options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
 
-            if(QnA[currentQuestion].CorrectAnswer == i + 1)
+            if (QnA[currentQuestion].CorrectAnswer == i)
             {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
