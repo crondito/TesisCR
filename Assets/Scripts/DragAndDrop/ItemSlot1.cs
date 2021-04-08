0289426869787309ba98bdb8e672c6e0f136cbe9
Carlos Rodas <crods57@gmail.com>
2021-04-08 12:45:57 -0500

    Empezando panel 10

---
+++ Assets/Scripts/DragAndDrop/ItemSlot1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ItemSlot1 : MonoBehaviour, IDropHandler
+{
+    [SerializeField] GameObject button;
+    [SerializeField] GameObject desaparece1;
+    [SerializeField] GameObject aparece1;
+    [SerializeField] GameObject desaparece2;
+    [SerializeField] GameObject aparece2;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log("On Drop");
+        if(eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            
+            if (eventData.pointerDrag.gameObject.tag == "square")
+            {
+                button.SetActive(true);
+                desaparece1.SetActive(false);
+                aparece1.SetActive(true);
+            }
+            else if (eventData.pointerDrag.gameObject.tag == "circle")
+            {
+                desaparece2.SetActive(false);
+                aparece2.SetActive(true);
+            }
+
+        }
+        
+    }
+}
