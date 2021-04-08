ef44071e107c464bf9b300d56b281abd8e33ac08
Carlos Rodas <crods57@gmail.com>
2021-04-08 10:04:57 -0500

    Drag And Drop

--- Assets/Scripts/DragAndDrop/ItemSlot.cs
+++ Assets/Scripts/DragAndDrop/ItemSlot.cs
@@ -6,12 +6,17 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] GameObject button;
+
     public void OnDrop(PointerEventData eventData)
     {
         //Debug.Log("On Drop");
         if(eventData.pointerDrag != null)
         {
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            button.SetActive(true);
+            
         }
+        
     }
 }
