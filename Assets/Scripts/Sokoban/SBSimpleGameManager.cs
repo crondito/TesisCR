a6ace83827851e86dcf5c69b4c5a45b6fcd6693f
Carlos Rodas <crods57@gmail.com>
2021-05-16 10:52:12 -0500

    Correcciones ejemplo Sokoban fin

--- Assets/Scripts/Sokoban/SBSimpleGameManager.cs
+++ Assets/Scripts/Sokoban/SBSimpleGameManager.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 
 public class SBSimpleGameManager : MonoBehaviour
 {
-
     private bool m_ReadyForInput;
     public SBPlayer2 m_Player;
     public GameObject m_NextButton;
+
+    public Vector3 posPlayer = new Vector3(670f, 11f, 0f);
+    public Vector3 posBox1 = new Vector3(348f, -97f, 0f);
+    public Vector3 posBox2 = new Vector3(562f, 11f, 0f);
 
     private void Update()
     {
@@ -31,13 +34,20 @@ public class SBSimpleGameManager : MonoBehaviour
 
     public void ResetPlayer(GameObject p)
     {
-        Vector3 posP = new Vector3(670f, 11f, 0f);
+        Vector3 posP = posPlayer;
         p.transform.localPosition = posP;
     }
 
     public void ResetBox(GameObject b)
     {
-        Vector3 posP = new Vector3(348f, -97f, 0f);
+        Vector3 posP = posBox1;
+        b.transform.localPosition = posP;
+        b.GetComponent<Image>().color = Color.white;
+    }
+
+    public void ResetBox2(GameObject b)
+    {
+        Vector3 posP = posBox2;
         b.transform.localPosition = posP;
         b.GetComponent<Image>().color = Color.white;
     }
