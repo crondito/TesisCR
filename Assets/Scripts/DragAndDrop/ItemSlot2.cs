03f5066aa341c739dbc4dc2a81bbfdbfa63bf7fb
Carlos Rodas <crods57@gmail.com>
2021-05-24 18:12:58 -0500

    En medio de Ejemplo Abstracción

---
+++ Assets/Scripts/DragAndDrop/ItemSlot2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemSlot2 : MonoBehaviour, IDropHandler
+{
+    [SerializeField] GameObject self;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+
+        //Debug.Log("On Drop");
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            if (eventData.pointerDrag.gameObject.tag == self.tag)
+            {
+                self.GetComponent<Image>().color = Color.green;
+            } else
+            {
+                self.GetComponent<Image>().color = Color.red;
+            }
+        }
+    }
+}
